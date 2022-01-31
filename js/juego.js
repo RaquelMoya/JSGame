ce03981647e30a0392a69b17197df2390533eeb6
raquelMoya <rakelms90@gmail.com>
2022-01-31 13:45:29 +0100

    Añadida funcion cambiaPantalla

--- js/juego.js
+++ js/juego.js
@@ -6,6 +6,23 @@ const cleanGame = () => {
     ganador = "";
 }
 
+const cambiaPantalla = (cambio) => {
+
+    let pantallaDeseada = "screen" + cambio;
+
+    let arrayPantallas = ["screen1", "screen2"];
+
+    //Le decimos a filter que si encuentra el valor que le pasamos en el array, no lo incluya
+    arrayPantallas = arrayPantallas.filter(valor => !pantallaDeseada.includes(valor));
+    //Le ponemos display "block" a la pantalla deseada
+    document.getElementById(pantallaDeseada).style.display = "block";
+    //Recorremos el arrayPantallas para poner el display:none
+    for(let pantalla of arrayPantallas) {
+        document.getElementById(pantalla).style.display = "none";
+    }
+
+}
+
 //Algoritmo
 
 //Se puede hacer con un While que salga del bucle cuando el coche llegue a completar x metros.
