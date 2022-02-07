5c9ec0ae9e2c6143ddca73696b4a1b7568e619f9
raquelMoya <rakelms90@gmail.com>
2022-02-07 16:43:19 +0100

    añadidos fondos en screens

--- js/juego.js
+++ js/juego.js
@@ -30,7 +30,7 @@ let choose1 = (nPokemon) => {
 }
 
 const cleanGame = () => {
-    
+
         window.location.reload();
       
     cambiaPantalla(1);
@@ -84,7 +84,7 @@ const pelea= () => {
 
         cambiaPantalla(4);
         
-        winner.innerHTML = `The winner is Team 1 with ${team1.nombre}`;
+        winner.innerHTML = `<img class="winner" src="img/winner.png" alt="winner" > </br> The winner is Team 1 with ${team1.nombre}`;
     } 
     if(team2.vida <= 0) {
         ganador = team1;
@@ -92,7 +92,7 @@ const pelea= () => {
         console.log("ganador team2");
         cambiaPantalla(4);
 
-        winner.innerHTML = `The winner is Team 2 with ${team2.nombre}`;
+        winner.innerHTML = `<img class="winner" src="img/winner.png" alt="winner" > </br> The winner is Team 2 with ${team2.nombre}`;
     }
     
    
